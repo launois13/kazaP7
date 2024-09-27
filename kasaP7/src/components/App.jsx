a15c46d6cd5ec5fae590d3f7ee51898e61963a4d
launois13 <pascalelaunois@free.fr>
2024-09-27 15:51:44 +0200

    corrections environnement

---
+++ kasaP7/src/components/App.jsx
@@ -0,0 +1,13 @@
+import "../sass/main.scss"
+import Header from "./Header"
+
+function App() {
+  return (
+    <div className="app">
+      <Header/>
+     
+    </div>
+    
+  )
+}
+export default App
--- kasaP7/src/main.jsx
+++ kasaP7/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from "./components/Header.jsx"
+import './main.scss'
+import './sass/header.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
